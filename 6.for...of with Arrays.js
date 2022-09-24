0cf3e16b2017ab9fef18d590b602843965941165
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 16:26:54 +0530

    for of with arrays

---
+++ 6.for...of with Arrays.js
@@ -0,0 +1,21 @@
+// array
+const students = ['John', 'Sara', 'Jack'];
+
+// using for...of
+for ( let element of students ) {
+
+    // display the values
+    console.log(element);
+}
+
+
+let Bikes = ['pulsar','apache','xtreme']
+for (let element of Bikes) {
+    console.log(element);
+}
+// John
+// Sara
+// Jack
+// pulsar
+// apache
+// xtreme
