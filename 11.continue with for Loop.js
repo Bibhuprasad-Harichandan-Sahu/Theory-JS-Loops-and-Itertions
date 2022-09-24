763014d701fe384b65023020787fd8327a51dd7e
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 18:56:47 +0530

    continue with loop

---
+++ 11.continue with for Loop.js
@@ -0,0 +1,14 @@
+// program to print the value of i
+for (let i = 1; i <= 5; i++) {
+
+    // condition to continue
+    if (i == 3) {
+        continue;
+    }
+
+    console.log(i);
+}//output->
+// 1
+// 2
+// 4
+// 5
