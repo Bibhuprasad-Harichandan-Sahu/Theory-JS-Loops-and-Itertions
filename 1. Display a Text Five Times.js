5ab605ac3d096afb34838831844ce135c384a678
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 14:59:18 +0530

    using for loop

---
+++ 1. Display a Text Five Times.js
@@ -0,0 +1,28 @@
+// program to display text 5 times
+const n = 5;
+
+// looping from i = 1 to 5
+for (let i = 1; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
+
+
+let nums = 10;
+for (let i = 1; i <= nums; i++) {
+    console.log("i am a Full Stack Web Developer");
+}
+// output->I love JavaScript.
+// I love JavaScript.
+// I love JavaScript.
+// I love JavaScript.
+// I love JavaScript.
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
+// i am a Full Stack Web Developer
