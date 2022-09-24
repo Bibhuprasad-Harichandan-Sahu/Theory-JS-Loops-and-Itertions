9369e8b17910420c5cc8f6f50f2b5c2c5c84ab7d
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-24 16:06:38 +0530

    for in with Srtrings

---
+++ 4.for...in with Strings.js
@@ -0,0 +1,26 @@
+const salaries= {
+    Jack : 24000,
+    Paul : 34000,
+    Monica : 55000
+}
+
+// using for...in
+for ( let i in salaries) {
+
+    // add a currency symbol
+    let salary = "$" + salaries[i];
+
+    // display the values
+    console.log(`${i} : ${salary}`);
+}
+
+
+
+
+
+const string = 'code';
+
+// using for...in loop
+for (let i in string) {
+    console.log(string[i]);
+}
